d647c16f7f860ad9a6e4e088ec080a3bbf9ac9df
Hisashi Itoh <it.hisas@gmail.com>
2020-01-10 17:45:16 +0900

    update

--- python/q_larning/matrix/q_larning.py
+++ python/q_larning/matrix/q_larning.py
@@ -24,19 +24,19 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 # 定数定義
-GEN_MAX = 50  # 学習の繰り返し回数
+GEN_MAX = 1 # 学習の繰り返し回数
 STATE_NO = 64 # 状態(8x8 のstate) の数
 ACTION_NO = 4 # 行動の自由度(数)
 ALPHA = 0.1   # 学習係数
 GAMMA = 0.9   # 割引率
 EPSILON = 0.3 # 行動選択のランダム率
 REWARD = 10   # ゴール時の報酬
-GOAL = 6      # ゴールの状態の番号
-LEVEL = 2     # 枝分かれの深さ
+GOAL = 54      # ゴールの状態の番号
 UP = 0        # 上方向への行動
 DOWN = 1      # 下方向への行動
 LEFT = 2      # 左方向へ移動
 RIGHT = 3     # 右方向へ移動
+LEVEL = 512   # 枝分かれの深さ
 
 class QLarning(object):
     def __init__(self, logger, gen_max=GEN_MAX, state_no=STATE_NO,
@@ -58,36 +58,41 @@ class QLarning(object):
         self.left = left
         self.right = right
         self.level = level
-
-        # Q の値を乱数で初期化
-        # 2次元配列の形状は [7,2]
-        self.q = [[self._frand() for i in range(self.action_no)] for j in range(self.state_no)]
+        
+        # Q の値を乱数で初期化(境界条件を考慮)
+        # 2次元配列の形状は [64,4]
+        self.q = self._init_q() 
         
         # Q 値の履歴情報(グラフを作成するために必要)
         self.q_hist = []
 
     def _init_q(self):
         """Q の初期化(境界条件含む)"""
+        q = [[self._frand() for i in range(self.action_no)] for j in range(self.state_no)]
         for i in range(self.state_no):
             for j in range(self.action_no):
-                self.q[i][j] = self._frand()
                 if i <= 7:
                     # 上辺の境界条件
-                    self.q[i][self.up] = -100.0
+                    q[i][self.up] = 0.0
                 elif i >= 56:
                     # 下辺の境界条件
-                    self.q[i][self.down] = -100.0
+                    q[i][self.down] = 0.0
                 elif i % 8 == 0:
                     # 左辺の境界条件
-                    self.q[i][self.left] = -100.0
+                    q[i][self.left] = 0.0
                 elif i % 8 == 7:
                     # 右辺の境界条件
-                    self.q[i][self.right] = -100.0
+                    q[i][self.right] = 0.0
+        return q
         
     def _rand0or1(self):
         """0か1の数値の何方かを返却する乱数"""
         return random.randint(0, 1)
 
+    def _rand03(self):
+        """0から3の数値を返却する乱数"""
+        return random.randint(0, 3)
+    
     def _frand(self):
         """0～1の一様乱数"""
         return random.random()
@@ -98,23 +103,32 @@ class QLarning(object):
         # epsilon-greedy 法による行動選択
         if self._frand() < self.epsilon:
             # ランダムに行動を選択
-            action = self._rand0or1()
+            while True:
+                action = self._rand03()
+                # 境界条件でQ 値が負値であればループ
+                if self.q[state][action] > 0.0:
+                    return action
         else:
-            action = self._set_action_by_q(state)
-        return action
+            return self._set_action_by_q(state)
         
     def _set_action_by_q(self, state):
-        """状態が指定された場合にQが大きいactionを選択する"""
-        if self.q[state][self.up] > self.q[state][self.down]:
-            return self.up
-        else:
-            return self.down
-
+        """状態が指定された場合にQ が大きいactionを選択する"""
+        max_q = 0.0
+        max_action = 0
+        for action in range(self.action_no):
+            # UP,DOWN,LEFT,RIGHT の状態を探索
+            if self.q[state][action] > max_q:
+                # 各状態で最大のQを持つaction を保存
+                max_q = self.q[state][action]
+                max_action = action
+        return max_action
+    
     def _nexts(self, status, action):
         """行動(action)のよって状態(status)をupdateする"""
-        # この式は今の設定(bi-treeの状態じゃないといけない)
-        return 2 * status + 1 + action
-
+        # 8x8 のマトリクスなので以下の構造となる
+        next_state_matrix = [-8, 8, -1, 1]
+        return status + next_state_matrix[action]
+    
     def _update_q(self, status, status_next, action):
         """Q の値を更新する"""
         qv = None
@@ -123,6 +137,13 @@ class QLarning(object):
             qv = self.q[status][action] + self.alpha * (self.reward - self.q[status][action])
         else:
             # 報酬がない場合
+
+            print("***")
+            print(status)
+            print(status_next)
+            print(action)
+            print("***")
+            
             qv = self.q[status][action] + self.alpha * (self.gamma * self.q[status_next][self._set_action_by_q(status_next)] - self.q[status][action])
         return qv
             
@@ -130,7 +151,7 @@ class QLarning(object):
         """Q の値を出力するユーティリティメソッド"""
         s = 0
         for state in self.q:
-            x = "s:{} ↑: {} ↓:{}".format(s, state[self.up], state[self.down])
+            x = "s:{} ↑: {} ↓: {} ←: {} →: {}".format(s, state[self.up], state[self.down], state[self.left], state[self.right])
             print(x)
             s += 1
 
@@ -143,8 +164,6 @@ class QLarning(object):
         q_hist = np.asarray(self.q_hist).transpose(1,2,0)
         s = 0
         for up, down in q_hist:
-            #ax.plot(x, up, marker="o", label="s:{},up".format(s))
-            #ax.plot(x, down, marker="o", label="s:{},down".format(s))
             ax.plot(x, up, label="s:{},up".format(s))
             ax.plot(x, down, label="s:{},down".format(s))
             s += 1
@@ -167,12 +186,18 @@ class QLarning(object):
                 action = self._select_action(state)
                 self.logger.info("status: {} action: {}".format(state, action))
                 state_next = self._nexts(state, action)
-
+                
                 # Q の更新
                 self.q[state][action] = self._update_q(state, state_next, action)
                 
                 # 行動により次の状態へ遷移
                 state = state_next
+                
+                # ゴールしたら探索終了
+                if state == self.goal:
+                    print("goal")
+                    break
+            
             # Q の出力
             self.print_q_value()
             # Q の履歴を保存する(グラフ用)
